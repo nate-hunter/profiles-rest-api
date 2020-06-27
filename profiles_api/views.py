eb0517a42b6b913063834f3d99b0b049f050fe5d
Nate <hunter.j.nate@gmail.com>
2020-06-27 01:29:37 -0400

    added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 
 from rest_framework import viewsets
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -80,7 +83,7 @@ class HelloViewSet(viewsets.ViewSet):
 
     def retrieve(self, request, pk=None):
         """Handle getting an object its ID"""
-
+        
         return Response({'http_method': 'GET...'})
     
     def update(self, request, pk=None):
@@ -97,3 +100,14 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object its ID"""
         
         return Response({'http_method': 'DELETE...'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+
+
+
